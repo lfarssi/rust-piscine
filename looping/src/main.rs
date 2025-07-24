7e46440c445bf84bd470bb211584d1a754492dc5
lfarssi <medlfarssi10@gmail.com>
2025-07-24 12:18:04 +0100

    looping

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,13 +1,12 @@
 use std::io;
 fn main() {
-    let answer= "The letter e";
     let mut input = String::new();
     let mut trial=0;
     loop{
         println!("I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
         io::stdin().read_line(&mut input).expect("Failure");
         trial +=  1;
-        if input.trim() == answer{
+        if input.trim() == "The letter e"{
             println!("the number of trials : {}", trial);
             break;
         }
