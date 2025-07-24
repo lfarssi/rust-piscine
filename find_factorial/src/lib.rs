64c6bb4dbf011daa5f2c1cb1c5c34006760c408a
lfarssi <medlfarssi10@gmail.com>
2025-07-24 13:08:21 +0100

    fibonacci2

--- find_factorial/src/lib.rs
+++ find_factorial/src/lib.rs
@@ -1,3 +1,1 @@
-pub fn factorial(num: u64) -> u64 {
-    if num ==0 || num ==1{ 1 } else {factorial(num-1)*num}
-}
+pub fn factorial(num: u64) -> u64 { if num ==0 || num ==1{ 1 } else {factorial(num-1)*num}}
