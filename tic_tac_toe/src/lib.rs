4a3105bbbfee55926aae5ef9553045c79a0dbeea
lfarssi <medlfarssi10@gmail.com>
2025-07-25 19:08:50 +0100

    tic_tac_toe

---
+++ tic_tac_toe/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn tic_tac_toe(table: [[char; 3]; 3]) -> String {
+}
+
+pub fn diagonals(player: char, table: [[char; 3]; 3]) -> bool {
+}
+
+pub fn horizontal(player: char, table: [[char; 3]; 3]) -> bool {
+}
+
+pub fn vertical(player: char, table: [[char; 3]; 3]) -> bool {
+}
