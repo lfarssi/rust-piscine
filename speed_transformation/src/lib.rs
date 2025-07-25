7722b45c9c38059801a64d6d9783010936c761ed
lfarssi <medlfarssi10@gmail.com>
2025-07-25 10:59:10 +0100

    borrow

--- speed_transformation/src/lib.rs
+++ speed_transformation/src/lib.rs
@@ -2,13 +2,4 @@ pub fn km_per_hour_to_meters_per_second(km_h: f64) -> f64 {
     km_h*(1000.0/3600.0)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -7,13 +7,3 @@ pub fn celsius_to_fahrenheit(c: f64) -> f64 {
 
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
