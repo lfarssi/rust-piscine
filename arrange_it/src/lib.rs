dcf44d1641de298c98027835c6e0c39dc01d35bb
lfarssi <medlfarssi10@gmail.com>
2025-07-26 13:11:21 +0100

    arrage_it

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -20,11 +20,12 @@ fn sort(mut arr: Vec<(String,String)>)->String{
     for i in 0..arr.len(){
         for j in 0..arr.len()-1-i{
             let a :i32= arr[j].1.parse().unwrap();
-            let b=arr[j+1].1.parse().unwrap();
+            let b :i32 =arr[j+1].1.parse().unwrap();
             if a>b{
                 arr.swap(j,j+1);
             }
         }
     }
+
     arr.into_iter().map(|(mot, _)| mot).collect::<Vec<String>>().join(" ")
 }
