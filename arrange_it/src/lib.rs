89540d5d6f4085c51c1c1bacc471fb55058143aa
lfarssi <medlfarssi10@gmail.com>
2025-07-26 13:09:48 +0100

    arrage_it

---
+++ arrange_it/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn arrange_phrase(phrase: &str) -> String {
+    let words =phrase.split_whitespace();
+    let mut arr_tup: Vec<(String,String)>=vec![];
+    for word in words{
+        let mut klma= String::new();
+        let mut nbr=' ';
+        for ch in word.chars(){
+            if ch.is_digit(10){
+                nbr=ch;
+            }else{
+                klma.push(ch);
+            }
+        }
+        arr_tup.push((klma, nbr.to_string()))
+    }
+    // println!("{:?}",arr_tup);
+    sort(arr_tup)
+}
+fn sort(mut arr: Vec<(String,String)>)->String{
+    for i in 0..arr.len(){
+        for j in 0..arr.len()-1-i{
+            let a :i32= arr[j].1.parse().unwrap();
+            let b=arr[j+1].1.parse().unwrap();
+            if a>b{
+                arr.swap(j,j+1);
+            }
+        }
+    }
+    arr.into_iter().map(|(mot, _)| mot).collect::<Vec<String>>().join(" ")
+}
