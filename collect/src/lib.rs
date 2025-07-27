eda80550a7a073878132eca474b789bfadb493dc
lfarssi <medlfarssi10@gmail.com>
2025-07-27 16:33:59 +0100

    collect

--- collect/src/lib.rs
+++ collect/src/lib.rs
@@ -1,8 +1,10 @@
 pub fn bubble_sort(arr : &mut [i32]){
-    // for i in 0..arr.len(){
-    //     for j in 0..arr.len()-1-i{
-    //         if arr[i]
-    //     }
-    // }
-    arr.sort()
+    for i in 0..arr.len(){
+        for j in 0..arr.len()-1-i{
+            if arr[j]>arr[j+1]{
+                arr.swap(j,j+1);
+            }
+        }
+    }
+    // arr.sort()
 }
