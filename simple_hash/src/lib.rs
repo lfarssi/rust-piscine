a197bf10f518bcc1ca0098205470a2b7928aca8e
lfarssi <medlfarssi10@gmail.com>
2025-07-27 13:06:22 +0100

    simple_hash

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::collections::HashMap;
+
+pub fn word_frequency_counter<'a>(words: &[&'a str]) -> HashMap<&'a str, usize> {
+    let mut map :HashMap<&'a str, usize> = HashMap::new();
+    for word in words.iter(){
+        *map.entry(*word).or_insert(0)+=1;
+    }
+    map
+    
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
+    frequency_count.len()
 }
