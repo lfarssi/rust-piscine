ba297881aee836859ae3c795829c92d52c98b0a0
lfarssi <medlfarssi10@gmail.com>
2025-07-27 11:59:56 +0100

    edit_distance

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -1,34 +1,18 @@
 pub fn edit_distance(source: &str, target: &str) -> usize {
-    let m = source.len();
-    let n = target.len();
-    
-    let mut dp = vec![vec![0; n + 1]; m + 1];
-
-    for i in 0..=m {
-        dp[i][0] = i; 
+    let src_ln = source.len();
+    let trg_ln = target.len();
+    let mut dp = vec![vec![0;trg_ln+1];src_ln+1];
+    for i in 0..=src_ln{
+        dp[i][0]=i;
     }
-    for j in 0..=n {
-        dp[0][j] = j; 
+    for j in 0..=trg_ln{
+        dp[0][j]=j;
     }
-
-    for i in 1..=m {
-        for j in 1..=n {
-            let cost = if source.as_bytes()[i - 1] == target.as_bytes()[j - 1] {
-                0 
-            } else {
-                1 
-            };
-            
-            dp[i][j] = *[
-                dp[i - 1][j] + 1, 
-                dp[i][j - 1] + 1, 
-                dp[i - 1][j - 1] + cost, 
-            ]
-            .iter()
-            .min()
-            .unwrap();
+    for i in 1..=src_ln{
+        for j in 1..=trg_ln{
+            dp[i][j]=*[dp[i-1][j]+1, dp[i][j-1]+1,dp[i-1][j-1]+if source.chars().nth(i-1)==target.chars().nth(j-1){0}else{1}].iter().min().unwrap();
         }
     }
-
-    dp[m][n] 
+    // println!("{:?}",dp);
+    dp[src_ln][trg_ln]
 }
