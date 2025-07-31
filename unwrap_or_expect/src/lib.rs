4508318204e0caf4293fc6311ce5c2aaec1f4510
lfarssi <medlfarssi10@gmail.com>
2025-07-31 14:37:10 +0100

    unwrap_or_expect

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -8,7 +8,12 @@ pub enum Security {
 
 pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String {
     match server{
-        Ok(res)=>res.to_string(),
+        Ok(res)=>{
+            match security_level{
+                Security::UnexpectedUrl=>panic!("{}",res),
+                _=>res.to_string(),
+            }
+        },
         Err(err)=> {
             match security_level {
                 Security::Unknown=>panic!("called `Result::unwrap()` on an `Err` value: \"{}\"", err),
