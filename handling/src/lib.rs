9cbbe4e8bc65d1d12fe4ee4f7ce1b9bc279115a7
lfarssi <medlfarssi10@gmail.com>
2025-07-31 18:53:07 +0100

    handling

---
+++ handling/src/lib.rs
@@ -0,0 +1,11 @@
+use std::path::Path;
+use std::io::Write;
+ use std::fs::OpenOptions;
+pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
+    let file= OpenOptions::new().read(true).create(true).append(true).open(path);
+    let _=match file{
+        Ok(mut res)=>writeln!(res, "{}",content),
+        Err(e)=> panic!("Failed to open or create the file: {}",e),
+ 
+    };
+}
