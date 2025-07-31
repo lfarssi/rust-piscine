038774f4ea122063a448d2607380c4246fab024a
lfarssi <medlfarssi10@gmail.com>
2025-07-31 14:15:42 +0100

    unwrap_or_expect

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -13,7 +13,7 @@ pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> Strin
             match security_level {
                 Security::Unknown=>panic!("called `Result::unwrap()` on an `Err` value: \"{}\"", err),
                 Security::Message=>panic!("ERROR: program stops"),
-                Security::Warning=>"WARNING: check the server".to_string(),
+                Security::Warning=>panic!("WARNING: check the server"),
                 Security::NotFound=>format!("Not found: {}",err),
                 Security::UnexpectedUrl=>panic!("{}",err)
             }
