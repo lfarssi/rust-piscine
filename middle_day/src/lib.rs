fad99e100e27e9078474ce707693c3d6ade7cd89
lfarssi <medlfarssi10@gmail.com>
2025-08-02 11:26:38 +0100

    middle_day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,10 +1,13 @@
-use chrono::{NaiveDate, Weekday,Datelike};
-pub fn middle_day(year: u32) -> Option<::Weekday> {
-    let days_in_year= if NaiveDate::from_ymd(year as i32,12;31).is_leap_year(){366} else{365};
-    if days_in_year %2 {
-        return None;
+use chrono::{NaiveDate,Weekday,Datelike};
+
+pub fn middle_day(year: u32) -> Option<Weekday> {
+    let leap_year= year%4==0 && (year%100!=0 || year%400==0);
+    let days_in_year=if leap_year{366}else{365};
+    if days_in_year%2==0{
+        None
+    } else {
+         let target  = NaiveDate::from_yo_opt(year as i32,days_in_year)?;
+         Some(target.weekday())
     }
-    let half_days = (days_in_year/2)+1;
-    let mid_date = NaiveDate::from_ymd(year as i32,1,1)+chrono::Duration.days(half_days as i32 -1);
-    Some(mid_date.weekday())
+
 }
