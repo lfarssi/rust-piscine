6536d739a988e4af767617f84ba026d225001513
lfarssi <medlfarssi10@gmail.com>
2025-08-02 17:02:06 +0100

    shopping_mall

--- shopping_mall/src/lib.rs
+++ shopping_mall/src/lib.rs
@@ -20,24 +20,25 @@ pub fn biggest_store(mall: &Mall)-> (String, Store){
 }
 
 
-pub fn highest_paid_employee(mall: &Mall) -> Vec<(String, Employee)> {
+pub fn highest_paid_employee(mall: &Mall) -> Vec<(&String, &Employee)> {
     let mut highest_salary = 0.0;
-    let mut n = String::new();
-    let mut res = Employee { age: 0, working_hours: (0,0), salary: 0.0 };
+    // These are Option because before we see any employee we don't have anything to refer to.
+    // But you'll always assign them before returning, so no None in result.
+    let mut highest_employee: Option<(&String, &Employee)> = None;
 
     for floor in mall.floors.values() {
         for store in floor.stores.values() {
             for (name, employee) in &store.employees {
                 if employee.salary > highest_salary {
                     highest_salary = employee.salary;
-                    n = name.clone();
-                    res = employee.clone();
+                    highest_employee = Some((name, employee));
                 }
             }
         }
     }
 
-    vec![(n, res)]
+    // unwrap() is safe here because we must have at least one employee.
+    vec![highest_employee.unwrap()]
 }
 
 
