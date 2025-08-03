24fc06624fc9be69ea1c7ac0d0ebe33bd33c53f5
lfarssi <medlfarssi10@gmail.com>
2025-08-03 11:31:10 +0100

    ordinal

--- ordinal/src/lib.rs
+++ ordinal/src/lib.rs
@@ -1,9 +1,12 @@
 pub fn num_to_ordinal(x: u32) -> String {
-    let y =x%10;
-    match y{
-        1=>x.to_string()+"st",
-        2=>x.to_string()+"nd",
-        3=>x.to_string()+"rd",
-        _=>x.to_string()+"th",
+    if x%100 >=11 && x%100<=13{
+        x.to_string()+"th"
+    } else{
+        match x%10{
+            1 =>x.to_string()+"st",
+            2 =>x.to_string()+"nd",
+            3 =>x.to_string()+"rd",
+            _ =>x.to_string()+"th",
+        }
     }
 }
