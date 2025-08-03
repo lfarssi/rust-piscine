9b8555ae0b7e501c11c7f86135d639e7d1fe6c34
lfarssi <medlfarssi10@gmail.com>
2025-08-03 11:40:01 +0100

    pangram

---
+++ pangram/src/lib.rs
@@ -0,0 +1,11 @@
+ use std::collections::HashSet;
+pub fn is_pangram(s: &str) -> bool {
+    let mut map = HashSet::new();
+    for c in s.to_lowercase().chars(){
+        if c.is_alphabetic(){
+        map.insert(c);
+        }
+    }
+    // println!("{}",map.len());
+    map.len()==26
+}
