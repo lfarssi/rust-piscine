228e5fe5e035ca69b46ecedd47a035843607a26f
lfarssi <medlfarssi10@gmail.com>
2025-08-03 17:28:49 +0100

    sc

--- scytale_cipher/src/lib.rs
+++ scytale_cipher/src/lib.rs
@@ -1,4 +1,7 @@
 pub fn scytale_cipher(message: &str, i: usize) -> String {
+    if message.is_empty(){
+        return message;
+    }
     let t9simat =(message.len()+i-1)/i;
     let mut table:Vec<Vec<char>> =vec![vec![' ';i];t9simat];
     let mut res = String::new();
