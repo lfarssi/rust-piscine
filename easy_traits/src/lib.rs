ef038e1ef8a9e33d42a5caac1fc97f6f97bc7bde
lfarssi <medlfarssi10@gmail.com>
2025-08-09 13:25:10 +0100

    easy_traits

---
+++ easy_traits/src/lib.rs
@@ -0,0 +1,34 @@
+#[derive(Clone)]
+pub struct StringValue {
+    pub value: String,
+}
+
+pub trait AppendStr {
+    fn append_str(&mut self, str_to_append: String) -> Self;
+
+    fn append_number(&mut self, nb_to_append: f64) -> Self;
+
+    fn remove_punctuation_marks(&mut self) -> Self;
+}
+
+impl AppendStr for StringValue {
+    fn append_str(&mut self, str_to_append:String)->Self{
+        self.value.push_str(&str_to_append);
+        Self {
+            value:self.value.clone(),
+        }
+    }
+    fn append_number(&mut self, nb_to_append:f64)-> Self{
+        self.value.push_str(&nb_to_append.to_string());
+        Self {
+            value:self.value.clone(),
+        }
+    }
+    fn remove_punctuation_marks(&mut self)->Self{
+        self.value =self.value
+        .chars().filter(|c| !c.is_ascii_punctuation()).collect::<String>();
+        Self{
+            value:self.value.clone(),
+        }
+    }
+}
