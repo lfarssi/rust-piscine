d85b1bc094ac547eedd2c46f7fa0cca93a022e74
lfarssi <medlfarssi10@gmail.com>
2025-08-11 18:45:30 +0100

    commits_stats

--- commits_stats/src/lib.rs
+++ commits_stats/src/lib.rs
@@ -1,30 +1,30 @@
 use std::collections::HashMap;
-// use json::JsonValue;
+use json::JsonValue;
 use chrono::{DateTime, Utc, Datelike};
 
-pub fn commits_per_author(data: &json::JsonValue) -> HashMap<String, u32> {
-    let mut map= HashMap::new();
-    for member in data.members() {
-        if let Some(login)= member["author"]["login"].as_str(){
-            map.entry(String::from(login)).and_modify(|v| *v+=1).or_insert(0);
+pub fn commits_per_author(data: &JsonValue) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
+    for commit in data.members() {
+        if let Some(login) = commit["author"]["login"].as_str() {
+            *map.entry(login.to_string()).or_insert(0) += 1;
         }
     }
-    map
 
+    map
 }
 
-pub fn commits_per_week(data: &json::JsonValue) -> HashMap<String, u32> {
+pub fn commits_per_week(data: &JsonValue) -> HashMap<String, u32> {
     let mut map = HashMap::new();
-    for member in data.members(){
-        if let Some(date)= member["commit"]["author"]["date"].as_str(){
-            if let Ok(parsed_date) = DateTime::parse_from_rfc3339(date) {
-
-               let date_utc = parsed_date.with_timezone(&Utc);
+    for commit in data.members() {
+        if let Some(date_str) = commit["commit"]["author"]["date"].as_str() {
+            if let Ok(date) = DateTime::parse_from_rfc3339(date_str) {
+                let date_utc: DateTime<Utc> = date.with_timezone(&Utc);
                 let iso_week = date_utc.iso_week();
                 let key = format!("{}-W{}", iso_week.year(), iso_week.week());
-                map.entry(key).and_modify(|v| *v+=1).or_insert(0);
+                *map.entry(key).or_insert(0) += 1;
             }
         }
     }
+
     map
 }
