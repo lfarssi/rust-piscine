15e1f64e11574cdf428b5b52ba05710dc62f99df
lfarssi <medlfarssi10@gmail.com>
2025-08-15 14:12:43 +0100

    border_cross

--- delete_prefix/src/lib.rs
+++ delete_prefix/src/lib.rs
@@ -1,7 +1,4 @@
 pub fn delete_prefix<'a>(prefix: &'a str,mut s: &'a str) -> Option<&'a str> {
-    if !s.contains(prefix){
-        return None;
-    }
     if s.starts_with(prefix){
         s=s.strip_prefix(prefix)?;
     }
