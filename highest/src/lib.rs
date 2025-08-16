35baacab58d31f63263b9d3e090e7efbce571fc6
lfarssi <medlfarssi10@gmail.com>
2025-08-16 17:05:47 +0100

    highest

--- highest/src/lib.rs
+++ highest/src/lib.rs
@@ -15,7 +15,7 @@ impl<'a> Numbers<'a> {
     }
 
     pub fn latest(&self) -> Option<u32> {
-        self.numbers[self.numbers.len()-1].copied()
+        self.numbers.last().copied()
     }
 
     pub fn highest(&self) -> Option<u32> {
